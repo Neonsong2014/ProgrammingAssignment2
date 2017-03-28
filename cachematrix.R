fd88b453c4b8974c7ad75719852dbc7f3636c6c7
Jian Song <jian.song12@gmail.com>
2017-03-28 00:06:47 -0700

    -off-by: Jian Song <jian.song12@gmail.com>
    i

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## this function is to create and store a special matrix object that can store its inverse matrix.
 
+makeCacheMatrix <- function(x = matrix()) {
+      
+      ## initialize inverse matrix variable 
+      ## that will be used to cache its inverse matrix 
+      invMtr <- NULL
+      
+      ## setter to replace existing matrix with a new one and remove existing inverse matrix
+      set <- function (mtr) {
+            invMtr <<- NULL
+            x <<- mtr
+      }
+      
+      ## getter to get stored matrix
+      get <- function() x
+      ## setter to replace existing inverse matrix with new one
+      setInvMtr <- function(rvMtr) invMtr <<-rvMtr
+      ## getter to get existing inverse matrix
+      getInvMtr <- function() invMtr
+      ## return a list for accessing the exectured closure and its values.
+      list ( set=set,
+             get=get,
+             setInvMtr = setInvMtr,
+             getInvMtr = getInvMtr)
 }
 
 
 ## Write a short comment describing this function
-
+## This function is to retrieve if cached the inverse matrix for a given matrix created by calling makeCachedMatrix function above.
+## Otherwise it calcuates the inverse matrix for the given matrix, cache the resulting inverse matrix and then return it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      invMtr <- x$getInvMtr()
+      ## check if inverse matrix has been calculated.  
+      ## if calculated already then returns the cached. 
+      ## Otherwise it means that the matrix has been changed.
+      if (!is.null(invMtr)){
+            return (invMtr)
+      }
+      ## get existing matrix
+      m <- x$get()
+      ## calculate its inverse matrix
+      invMtr <- solve(m)
+      ## cache the calculated inverse matrix
+      x$setInvMtr(invMtr)
+      ## return calculated inverse matix
+      invMtr
+      
 }
