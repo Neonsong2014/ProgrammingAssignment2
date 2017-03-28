6c1859a17e9fc79874467a6d3506afeb1887c42c
Jian Song <jian.song12@gmail.com>
2017-03-28 00:41:07 -0700

    completed week3 programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
 
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions, makeCacheMatrix, cacheSolve are designed to complete the following tasks:
+## makeCacheMatrix - 
+## 1. take a square numeric matrix as input parameter
+## 2. create a variable in the closure to allow its later calculated inverse matrix to be cached.
+## 3. create two setters and two getters to cache/retrieve the matrix and its inverse matrix
+## 4. return a list that contains setting and getting functions.
+
+## cacheSolve - 
+## 1. take the special matrix created by calling makeCacheMatrix function as its input parameter
+## 2. check if there exists an inverse matrix
+## 3. if true returns the inverse matrix
+## 4. if not then retrieve the matrix and call solve function to calculate its inverse matrix.
+## 5. cache the resulting inverse matrix
+## 6. return the resulting inverse matrix.
 
 ## Write a short comment describing this function
 
-## this function is to create and store a special matrix object that can store its inverse matrix.
+## this function is to create a special matrix object that can store its inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
       
@@ -33,7 +45,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## This function is to retrieve if cached the inverse matrix for a given matrix created by calling makeCachedMatrix function above.
+## This function is to retrieve if cached the inverse matrix for a given matrix created by calling makeCacheMatrix function above.
 ## Otherwise it calcuates the inverse matrix for the given matrix, cache the resulting inverse matrix and then return it.
 cacheSolve <- function(x, ...) {
       ## Return a matrix that is the inverse of 'x'
@@ -48,7 +60,7 @@ cacheSolve <- function(x, ...) {
       m <- x$get()
       ## calculate its inverse matrix
       invMtr <- solve(m)
-      ## cache the calculated inverse matrix
+      ## cache the calculated inverse matrix in the clourse created by makeCacheMatrix
       x$setInvMtr(invMtr)
       ## return calculated inverse matix
       invMtr
